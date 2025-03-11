799c2ada8874fbeca648457e0ad3ab896015b5fb
Tiago <tiagohig70@gmail.com>
2025-03-10 21:00:10 -0500

    New function in views

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+def index(request):
+    return HttpResponse('<h1>Index page</h1>')
+
+
+def hello(request, username):
+    return HttpResponse('<h2>Hello %s</h2>' % username)
+
 
 def about(request):
     return HttpResponse('About')
