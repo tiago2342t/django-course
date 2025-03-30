d1d73881657ea764ebcd5118b75360a49788cee6
Tiago <tiagohig70@gmail.com>
2025-03-30 16:45:29 -0500

    Admin user and templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('about/', views.about),
-    path('hello/<str:username>', views.hello)
+    path('hello/<str:username>', views.hello),
+    path('projects/', views.projects),
+    path('tasks/', views.tasks)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,24 @@
 from django.http import HttpResponse
+from .models import Project, Task
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Index page</h1>')
+    return render(request, 'index.html')
 
+def about(request):
+    return render(request, 'about.html')
 
 def hello(request, username):
     return HttpResponse('<h2>Hello %s</h2>' % username)
 
+def projects(request):
+    return render(request, 'projects.html')
+    # projects = list(Project.objects.values())
+    # return JsonResponse(projects, safe=False)
 
-def about(request):
-    return HttpResponse('About')
+def tasks(request):
+    return render(request, 'tasks.html')
+    # task = Task.objects.get(title=title)
+    # task = get_object_or_404(Task, id=id)
+    # return HttpResponse('Task: %s'% task.title)
