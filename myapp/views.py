bbe6cc4421f8623a16c9bb2e6faf1b3880779919
Tiago <tiagohig70@gmail.com>
2025-04-01 22:29:22 -0500

    New features in template

--- myapp/views.py
+++ myapp/views.py
@@ -4,21 +4,33 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    title = 'Django course!!'
+    return render(request, 'index.html', {
+        'title': title
+    })
 
 def about(request):
-    return render(request, 'about.html')
+    username = 'santi'
+    return render(request, 'about.html', {
+        'username': username
+    })
 
 def hello(request, username):
     return HttpResponse('<h2>Hello %s</h2>' % username)
 
 def projects(request):
-    return render(request, 'projects.html')
     # projects = list(Project.objects.values())
+    projects=Project.objects.all()
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
     # return JsonResponse(projects, safe=False)
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
     # task = Task.objects.get(title=title)
     # task = get_object_or_404(Task, id=id)
     # return HttpResponse('Task: %s'% task.title)
